2ba6a55400e323950160d09939742f9eecb1822e
LeNgocVanAnh <anhle161108@gmail.com>
2018-09-10 20:37:55 +0700

    add db entities framwork code first for data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    // tự động hủy
+    public class Disposable : IDisposable
+    {
+        private bool isDisposed;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+            // thu hoạch bộ nhớ
+        }
+
+        private void Dispose (bool disposing)
+        {
+            if(!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
